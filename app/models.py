f1edcb0b2f140333ef9c5b3244c8bf75893786de
manvithapula <manvithapula@gmail.com>
2025-07-20 20:22:33 +0530

    fix: type error

--- app/models.py
+++ app/models.py
@@ -5,13 +5,13 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 class Board(Base):
-    __tablename__ = "boards"
+    _tablename_ = "boards"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), unique=True, index=True)
-    columns = relationship("Column", back_populates="board")
+    columns = relationship("KanbanColumn", back_populates="board")
 
-class Column(Base):
-    __tablename__ = "columns"
+class KanbanColumn(Base):
+    _tablename_ = "columns"
     column_str_id = Column(String(100), primary_key=True, index=True)
     name = Column(String(100))
     order_on_board = Column(Integer)
@@ -20,10 +20,10 @@ class Column(Base):
     tasks = relationship("Task", back_populates="column")
 
 class Task(Base):
-    __tablename__ = "tasks"
+    _tablename_ = "tasks"
     task_str_id = Column(String(100), primary_key=True, index=True)
     title = Column(String(100))
     description = Column(String(255), nullable=True)
     order_in_column = Column(Integer)
     column_str_id = Column(String(100), ForeignKey("columns.column_str_id"))
-    column = relationship("Column", back_populates="tasks")
+    column = relationship("KanbanColumn", back_populates="tasks")
